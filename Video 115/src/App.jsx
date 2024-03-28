ab7f4b97668ed67715abf2eeb2e091d34f3241a3
Hafijur Rahman <hafijur021998@gmail.com>
2024-03-28 11:16:31 +0530

    react-router

--- Video 115/src/App.jsx
+++ Video 115/src/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import About from "./components/About";
 
-function App() { 
-
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path:'/about',
+      element: <About />,
+    }
+  ]);
   return (
-    <> 
-    <Navbar/>
+    <>
+      <Navbar />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Video 115/src/components/Navbar.jsx
+++ Video 115/src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return (
-    <div>
-      <nav> 
-        <a href=""><li>Home</li></a>
-        <a href=""><li>About</li></a>
-        <a href=""><li>Contact</li></a>
-      </nav>
-    </div>
-  );
+    return (
+        <div>
+            <nav>
+                <a href="/">
+                    <li>Home</li>
+                </a>
+                <a href="/about">
+                    <li>About</li>
+                </a>
+                <a href="/login">
+                    <li>Login</li>
+                </a>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;
